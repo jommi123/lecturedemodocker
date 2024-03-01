8b70f56cb4b017a8ce81a5b0aa32b4264db4c55b
joromm <joromm2@gmail.com>
2024-03-01 13:38:45 +0200

    Second commit / addMe function is added

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,10 +4,17 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+    public App() {
+
+    }
+
+    public static int addMe(int a, int b) {
+        return a + b;
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!   " + addMe(3,232));
     }
 }
